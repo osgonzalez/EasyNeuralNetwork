60221caa4600e1af23aef1af283046a4a5938558
Oscar Servia Gonzalez <osgonzalez@esei.uvigo.es>
2019-08-14 04:11:31 +0200

    Add Templates Folder

--- ENNApp/urls.py
+++ ENNApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('prueba/<int:numero>', views.vistaPrueba, name='vistaPrueba'),
+    path('<int:question_id>/', views.detail, name='detail'),
+
 ]
--- ENNApp/views.py
+++ ENNApp/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
+
+
+# Import Models
+from .models import Question
+
+
+# Views 
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+def vistaPrueba(request, numero):
+    context = {
+        'number': numero,
+    }
+    return render(request, 'ENNApp/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'ENNApp/detail.html', {'question': question})
